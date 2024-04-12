f5774633b75ea121db0f5ba23a77d868e1592aae
dachim <mefarishvilidachi35@gmail.com>
2024-04-13 01:08:01 +0400

    commit

---
+++ Day 02/Homework/hw.py
@@ -0,0 +1,10 @@
+"""print("გავიცანი და დავუმეგობრდი:  " ) ჩამოწერეთ ყველა ვისაც დაუმეგობრდით 
+
+
+print("რას ვფიქრობ GOAზე" ) 
+დაპრინტეთ თქვენი აზრი გოაზე,"""
+
+
+print("gavicani da daumegobrdi luka gurgenizes")
+
+print("GOA aris yvelaze kargi akademia aq marto programirebas ar swavlob aq cxovrebasac swavlob")
---
+++ Day 04/Homework/task.py
@@ -0,0 +1,16 @@
+"""ამოცანა:
+თქვენი ოჯახის წევრების ასაკები ჩამოწერეთ ცვლადების სახით
+
+დაპრინტეთ მათი ასაკები ხელმეორედ, 2 წლის შემდეგ"""
+
+qeti = 35 
+
+dato = 37 
+
+julieta = 56
+
+mamuka = 58
+
+print(qeti, dato, julieta, mamuka)
+
+print(qeti + 2, dato + 2, julieta + 2, mamuka + 2)
